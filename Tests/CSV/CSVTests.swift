5a1e66c379491a5c4a519f2771c55ef138215948
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-06-11 18:31:29 +0900

    Add test for #1

--- Tests/CSV/CSVTests.swift
+++ Tests/CSV/CSVTests.swift
@@ -69,4 +69,19 @@ class CSVTests: XCTestCase {
         XCTAssertEqual(i, 3)
     }
     
+    func testSample5() {
+        let csv = "\"abc\",1,\"2\"\n\n\"cde\",3,\"4\""
+        var i = 0
+        for row in try! CSV(string: csv) {
+            switch i {
+            case 0: XCTAssertEqual(row, ["abc", "1", "2"])
+            case 1: XCTAssertEqual(row, [""])
+            case 2: XCTAssertEqual(row, ["cde", "3", "4"])
+            default: break
+            }
+            i += 1
+        }
+        XCTAssertEqual(i, 3)
+    }
+    
 }
