764201494a90de2f3a49065ea0b43e3c7a1fe775
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-06-11 12:48:48 +0900

    Add iOS target for Carthage

---
+++ Sources/CSV.h
@@ -0,0 +1,12 @@
+//
+//  CSV.h
+//  CSV
+//
+//  Created by Yasuhiro Hatta on 2016/06/11.
+//
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double CSVVersionNumber;
+FOUNDATION_EXPORT const unsigned char CSVVersionString[];
