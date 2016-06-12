47f4cad5aaf2f35a9a0b0b5d231f04f332aa8194
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2016-06-13 01:04:31 +0900

    Add more unit tests

---
+++ Tests/CSV/ReadmeTests.swift
@@ -0,0 +1,60 @@
+//
+//  ReadmeTests.swift
+//  CSV
+//
+//  Created by Yasuhiro Hatta on 2016/06/13.
+//  Copyright © 2016 yaslab. All rights reserved.
+//
+
+import XCTest
+@testable import CSV
+
+class ReadmeTests: XCTestCase {
+    
+    func testFromCSVString() {
+        for row in try! CSV(string: "1,foo\n2,bar") {
+            print("\(row)")
+            // => ["1", "foo"]
+            // => ["2", "bar"]
+        }
+    }
+    
+    func testFromFilePath() {
+        //for row in try! CSV(path: "/path/to/file.csv") {
+        //    print("\(row)")
+        //}
+    }
+    
+    func testGettingTheHeaderRow() {
+        let csv = try! CSV(
+            string: "id,name\n1,foo\n2,bar",
+            hasHeaderRow: true) // default: false
+        
+        let headerRow = csv.headerRow!
+        print("\(headerRow)") // => ["id", "name"]
+        
+        for row in csv {
+            print("\(row)")
+            // => ["1", "foo"]
+            // => ["2", "bar"]
+        }
+    }
+    
+    func testGetTheFieldValueUsingSubscript() {
+        let csv = try! CSV(
+            string: "id,name\n1,foo",
+            hasHeaderRow: true) // It must be true.
+        
+        while csv.next() != nil {
+            print("\(csv["id"]!)")   // => "1"
+            print("\(csv["name"]!)") // => "foo"
+        }
+    }
+    
+    func testProvideTheCharacterEncoding() {
+        //let csv = try! CSV(
+        //    path: "/path/to/file.csv",
+        //    encoding: NSUTF8StringEncoding)
+    }
+    
+}
